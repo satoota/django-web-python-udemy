4b41e21dab03ee06706cd172e0cd4b68e450f421
Otávio Asatomi Silva Sato <otavio.sato@gmail.com>
2020-04-20 19:59:34 -0300

    adicionando fonte

---
+++ contas/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render, redirect
+from .models import Transacao
+from .form import TransacaoForm
+import datetime
+
+def home(request):
+    data = {}
+    data['transacoes'] = ['teste1', 'teste2', 'teste3', 'teste4']
+
+    data['now'] = datetime.datetime.now()
+    #html = "<html><body>It is now %s.</body></html>" % now
+    #return HttpResponse(html)
+    return render(request, 'contas/home.html', data)
+
+def listagem(request):
+    data = {}
+    data['transacoes'] = Transacao.objects.all()
+    return render(request, 'contas/listagem.html', data)
+
+def nova_transacao(request):
+    data = {}
+    form = TransacaoForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+        return redirect('url_listagem')
+
+    data['form'] = form
+    return render(request, 'contas/form.html', data)
